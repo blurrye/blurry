06a9451abb457bca5f598e7bdfd54ed43a5a5333
blurrye <blurrye@163.com>
2018-03-21 21:49:51 +0800

    models
    models.py, admin.py, 添加几条数据简单测试

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name = '分类'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name = '标签'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+    # 文章摘要
+    excerpt = models.CharField(max_length=200, blank=True)
+
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    # 文章作者,从 django.contrib.auth.models 导入
+    author = models.ForeignKey(User)
+
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = verbose_name
+        ordering = ['-created_time']
+
+    def __str__(self):
+        return self.title
