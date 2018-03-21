a7345646fe1a0c4d091157676a34daa23e2aea67
blurrye <blurrye@163.com>
2018-03-21 19:34:28 +0800

    favicon.ico

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
+from django.views.generic.base import RedirectView
+
 from blog import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico')),
 ]
