4fc776360444436fc619661765df1455ba9bb904
blurrye <blurrye@163.com>
2018-03-22 23:13:48 +0800

    TAKI
    人气博主的TAKI君（路人女主的养成方法第4话9:04）.
    布局大致完成,见detail页面.

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ from blog import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico')),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Post
 
@@ -6,3 +6,8 @@ from .models import Post
 def index(request):
     post_list = Post.objects.all()
     return render(request, 'blog/index.html', locals())
+
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/detail.html', locals())
